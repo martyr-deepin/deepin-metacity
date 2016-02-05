83e76b0df7a629b75fde508b4c9503f401ce4c19
Sian Cao <yinshuiboy@gmail.com>
2016-02-05 10:21:06 +0800

    improve copyright
    Change-Id: Ia68ae143ec80edc32c598e567eeadd0943e4195d

--- src/include/deepin-dbus-service.h
+++ src/include/deepin-dbus-service.h
@@ -1,21 +1,13 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
-/*
- * deepin-dbus-service.h
- * Copyright (C) 2015 Sian Cao <yinshuiboy@gmail.com>
+
+/**
+ * Copyright (C) 2015 Deepin Technology Co., Ltd.
  *
- * deepin metacity free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- * 
- * deepin-metacity is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along
- * with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+ **/
 
 #ifndef _DEEPIN_DBUS_SERVICE_H_
 #define _DEEPIN_DBUS_SERVICE_H_
