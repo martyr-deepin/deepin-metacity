84b2ae578712052dc98387eda81db2ff130604a2
Sian Cao <yinshuiboy@gmail.com>
2016-06-21 16:06:39 +0800

    Use Bamf to load icon first
    Change-Id: I44f1daa6ab14571657a02421e031de4770f5acc4

--- src/ui/deepin-tab-widget.c
+++ src/ui/deepin-tab-widget.c
@@ -14,6 +14,9 @@
 #include <math.h>
 #include <util.h>
 #include <gdk/gdk.h>
+#include <gio/gio.h>
+#include <gio/gdesktopappinfo.h>
+#include <libbamf/libbamf.h>
 #include "boxes.h"
 #include "../core/window-private.h"
 #include "deepin-tab-widget.h"
@@ -413,9 +416,58 @@ static void meta_deepin_tab_widget_class_init (MetaDeepinTabWidgetClass *klass)
   g_object_class_install_properties(gobject_class, N_PROPERTIES, property_specs);
 
 }
+static GdkPixbuf* get_icon_for_application (BamfApplication* app, MetaWindow* window)
+{
+    GdkPixbuf* image = NULL;
+    GtkIconTheme* theme = gtk_icon_theme_get_default();
 
-GtkWidget *
-meta_deepin_tab_widget_new (MetaWindow* window)
+    const gchar* desktop_filename = bamf_application_get_desktop_file(app);
+    if (desktop_filename) {
+        GIcon* icon = NULL;
+        GtkIconInfo* iconinfo = NULL;
+
+        GDesktopAppInfo* appinfo = g_desktop_app_info_new_from_filename(desktop_filename);
+        if (appinfo) {
+            icon = g_app_info_get_icon(appinfo);
+            if (icon) {
+                iconinfo = gtk_icon_theme_lookup_by_gicon(theme, icon, ICON_SIZE, 0);
+                if (iconinfo) {
+                    image = gtk_icon_info_load_icon(iconinfo, NULL);
+                }
+            }
+
+            g_object_unref(appinfo);
+        }
+        if (iconinfo) g_object_unref(iconinfo);
+
+        if (image) return image;
+    }
+
+    // get icon for application that runs under terminal through wnck
+    if (app && image == NULL) {
+        meta_verbose("WM_CLASS: %s, %s", window->res_name, window->res_class);
+
+        /* try to load icon from res_class first, cause window->icon may
+         * contain a broken one 
+         **/
+        char* icon_name = g_ascii_strdown(window->res_class, -1);
+        image = gtk_icon_theme_load_icon(theme, icon_name, ICON_SIZE, 0, NULL);
+        g_free(icon_name);
+    }
+
+    if (!image) {
+        image = gtk_icon_theme_load_icon(theme, "application-default-icon", ICON_SIZE, 0, NULL);
+    }
+
+    if (!image) {
+        image = gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, ICON_SIZE, ICON_SIZE);
+        gdk_pixbuf_fill(image, 0x00000000);
+    }
+
+    return image;
+}
+
+GtkWidget * meta_deepin_tab_widget_new (MetaWindow* window)
 {
   MetaDeepinTabWidget* widget;
 
@@ -435,32 +487,12 @@ meta_deepin_tab_widget_new (MetaWindow* window)
       widget->priv->outer_rect = *(MetaRectangle*)&mon_geom;
 
   } else {
-      meta_verbose("WM_CLASS: %s, %s", window->res_name, window->res_class);
+      BamfMatcher* matcher = bamf_matcher_get_default();
+      BamfApplication* app = bamf_matcher_get_application_for_xid(matcher, window->xwindow);
 
-      /* try to load icon from res_class first, cause window->icon may
-       * contain a broken one 
-       **/
-      GError* error = NULL;
-      char* icon_name = g_ascii_strdown(window->res_class, -1);
-      GdkPixbuf* icon = gtk_icon_theme_load_icon(
-              gtk_icon_theme_get_default(),
-              icon_name, ICON_SIZE, GTK_ICON_LOOKUP_USE_BUILTIN, &error);
-      g_free(icon_name);
-
-      if (icon == NULL) {
-          if (error) {
-              meta_verbose("%s", error->message);
-              g_error_free(error);
-          }
-
-          icon = window->icon;
-      }
-
-      GdkPixbuf* scaled = gdk_pixbuf_scale_simple (icon,
-              ICON_SIZE, ICON_SIZE, GDK_INTERP_BILINEAR);
-      widget->priv->icon = gdk_cairo_surface_create_from_pixbuf(scaled,
-                1.0, NULL);
-      g_object_unref(scaled);
+      GdkPixbuf* pixbuf = get_icon_for_application(app, window);
+      widget->priv->icon = gdk_cairo_surface_create_from_pixbuf(pixbuf, 1.0, NULL);
+      g_object_unref(pixbuf);
   }
 
   return (GtkWidget*)widget;
